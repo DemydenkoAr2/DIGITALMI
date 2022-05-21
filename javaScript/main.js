6c0bd959bd1ea50479d4c8cdf3684cc96ee669d4
Demidenkoar2 <lakai6667@gmail.com>
2022-05-21 22:38:30 +0300

    git remote add origin https://github.com/Demidenkoar2/DIGITALMI-master-.git

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,27 +1,33 @@
 const scrollTopbarBlock = document.getElementById('scroll');
-const scrollTopbarLinks = document.querySelectorAll('.a__ul-link') 
+const scrollTopbarLinks = document.querySelectorAll('.a__ul-link');
+
 let lastScrollPosition = 0;
+let screenWidth = 0;
 
 window.addEventListener('scroll', () => {
   lastScrollPosition = window.scrollY;
+  screenWidth = document.documentElement.clientWidth;
+  // console.log(screenWidth)
   event.preventDefault();
   if (lastScrollPosition > 1) {
-  scrollTopbarBlock.classList.add('active-scroll')
-  scrollTopbarBlock.classList.remove('background-header')
+    scrollTopbarBlock.classList.add('active-scroll')
+    scrollTopbarBlock.classList.remove('background-header')
 
     scrollTopbarLinks.forEach((el) => {
       el.classList.add('active-scroll-links');
     });
-  }else if (lastScrollPosition === 0) {
+  } else if (lastScrollPosition === 0 && screenWidth > 770) {
       scrollTopbarBlock.classList.add('background-header')
       scrollTopbarBlock.classList.remove('active-scroll')
 
       scrollTopbarLinks.forEach((el) => {
-      el.classList.remove('active-scroll-links');
-    });
-  };
+        el.classList.remove('active-scroll-links');
+      });
+    }else if (lastScrollPosition === 0) {
+      scrollTopbarBlock.classList.add('background-header')
+      scrollTopbarBlock.classList.remove('active-scroll')
+    }
 });
-
 
 const firstElementLink = document.getElementById('first-link')
 const secondElemetLink = document.getElementById('second-link')
@@ -63,5 +69,26 @@ elementEmail.addEventListener('click', () => {
   navigator.clipboard.writeText(elementEmail.innerHTML);
   elementEmailAlert.classList.add('span__alert-active')
     setTimeout (() => elementEmailAlert.classList.remove('span__alert-active'), 2000)
-})
+});
+
+const btnNavHiden = document.getElementById('btn-nav-hiden');
+const navBlock = document.getElementById('nav-container');
+
+btnNavHiden.addEventListener('click', () => {
+  navBlock.classList.toggle('nav-login-display');
+  navBlock.classList.toggle('nav-login-hiden');
+  navBlock.classList.toggle('top-active-bgcolor');
+  scrollTopbarBlock.classList.toggle('top-active-bgcolor');
+  
+  scrollTopbarLinks.forEach((el) => {
+    el.classList.add('active-scroll-links');
+  });
+});
+
+(function () {
+  const dash = document.querySelector('.button-dash');
+  dash.addEventListener('click', () => {
+    dash.classList.toggle('dash-active');
+  });
+}());
 
--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "lakai6667@gmail.com";
+    $to = "contact@digitalmi.net";
     $from = $_REQUEST['name'];
     $headers = "Content-type: text/html;From: $from";
 
@@ -15,46 +15,5 @@
     $body .= 'Message : '.$fields['message']. '<br>';
 
     $send = mail($to, $body, $headers);
-
-
-    // // Получим данные с формы
-
-    // $name = $_POST['name'];
-    // $email = $_POSY['email'];
-    // $massage = $_POST['massage'];
-
-    // // обработка данных 
-
-    // // Метод преобразование символов в сущности
-    // $name = htmlspecialchars($name);
-    // $email = htmlspecialchars($email);
-    // $massage = htmlspecialchars($massage);
-
-    // // Декодирование URL
-    // $name = urldecode($name);
-    // $email = urldecode($email);
-    // $massage = urldecode($massage);
-
-    // // метод удаление пробельных символов
-    // $name = trim($name);
-    // $email = trim($email);
-    // // ??? !!!
-    // $massage = trim($massage);
-    // // ??? !!!
-
-
-    // // отправляем данные на почту contact@digitalmi.net
-
-    // if(mail("mxnxchrxmx1@gmail.com",
-    //     "New massage",
-    //     "Name: ".$name."\n".,
-    //     "Email: ".$email."\n".
-    //     "Message: ".$massage."\n".))
-    //      {
-    //         echo('Massage true')
-    //     }
-    //     else {
-    //         echo('Massage false')
-    //     }
 ?>
 
